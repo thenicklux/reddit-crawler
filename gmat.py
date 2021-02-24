d3b2694306b70a531b447c3c41fadafe1825a4fe
thenicklux <79524356+thenicklux@users.noreply.github.com>
2021-02-24 14:34:02 -0500

    Add files via upload

---
+++ gmat.py
@@ -0,0 +1,23 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import praw
+import pandas as pd
+
+
+reddit = praw.Reddit(client_id='GdI-aMIryvtD3Q',
+                     client_secret='HcAiQe2qNP5vIPgZ3y9BFeTTrTc',
+                     password='****',
+                     user_agent='testscript by /u/licknux',
+                     username='licknux')
+
+posts = []
+ml_subreddit = reddit.subreddit('gmat')
+for post in ml_subreddit.hot(limit=450):
+    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+
+print('posts').encode('utf-8')
+
+posts.to_csv('gmat.csv', index=False)
